e79bf117afd7891c39d7fe48fae32be8d9fb9e72
Abhijeet Verma <104731892+eingelganger@users.noreply.github.com>
2023-06-15 15:46:10 +0530

    Update1 index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function makeSound(key)
 {
     switch (key) {
         case 'w':
-            var tom1= new Audio('sounds/tom-1.mp3');
+            var tom1= new Audio('tom-1.mp3');
             tom1.play();
             break;
         case 'a':
